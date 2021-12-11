d909e810ca69f8d9e0374817119edf962c9d01ba
LEKOSOV <74300147+LEKOSOV@users.noreply.github.com>
2021-12-11 22:43:55 +0300

    Delete fanplusone_new directory

--- fanplusone_new/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-	$('.burger-btn').on('click', function(e) {
-		e.preventDefault;
-		$(this).toggleClass('burger-btn_active');
-		$('.burger-navs').slideToggle(400).css('display', 'flex');
-	})
-})
